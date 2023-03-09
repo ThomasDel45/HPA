694eff239fa68f4703d3fc85254b944791d13350
Thomas Del Rosario <ThomasDel17@csu.fullerton.edu>
2023-03-08 20:39:53 -0800

    testing things

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 clickme.addEventListener('click',function() {
     alert('Testing if it works');
 })
+
+//maybe adding what house youre in.
+
+var name = window.prompt("What is your house");
+document.write("Hi" + name + "!");
